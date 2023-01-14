9170c33e66b5f6c0e03e0496684f7412c2530579
Kerby00 <119148777+Kerby00@users.noreply.github.com>
2023-01-14 01:36:16 -0700

    added lines of code that apply localstorage values to textarea

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 var currentDayDisplayEl = $('#currentDay')
 var currenthour = dayjs().hour()
 console.log(currenthour)
-//This function adds current day using dayjs/////
+//This function adds current date using dayjs/////
 $(function () {
   var DisplayDay = dayjs().format('dddd, MMMM DD, YYYY');
   currentDayDisplayEl.text(DisplayDay)
@@ -19,30 +19,38 @@ $(function () {
 //this is an event listener for clicking the save buttons///
 $('.saveinput').on('click', function () {
   //these traverse the dom to find the correct input values///
-  var inputval = $(this).siblings('.todo').val();
+  var inputval = $(this).siblings('.description').val();
   var idOfHour = $(this).parent().attr('id');
   //this adds the input value and key with an id of the hour blocks/////
   localStorage.setItem(idOfHour, inputval);
 });
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  $('.time-block').each(function(){
-    var blockhour = $(this).data('time');
-    console.log(blockhour);
-    if (blockhour < currenthour ) {
-      $(this).addClass('past')
-    } else if (blockhour > currenthour){
-      $(this).addClass('future')
-    } else {
-      $(this).addClass('present')
-    }
-  });
+// TODO: Add code to apply the past, present, or future class to each time
+// block by comparing the id to the current hour. HINTS: How can the id
+// attribute of each time-block be used to conditionally add or remove the
+// past, present, and future classes? How can Day.js be used to get the
+// current hour in 24-hour time?
+/// This function takes the value of data set and compares it to a 24 hour clock/value, then applies classes depending on the if statement///
+$('.time-block').each(function () {
+  var blockhour = $(this).data('time');
+  if (blockhour < currenthour) {
+    $(this).addClass('past')
+  } else if (blockhour > currenthour) {
+    $(this).addClass('future')
+  } else {
+    $(this).addClass('present')
+  }
+});
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
-  //
+  // These lines of code add the localStorage items that are linked to an id to the 'description' tag correlating to the same id///
+  $('#hour-9 .description').val(localStorage.getItem("hour-9"));
+  $('#hour-10 .description').val(localStorage.getItem("hour-10"));
+  $('#hour-11 .description').val(localStorage.getItem("hour-11"));
+  $('#hour-12 .description').val(localStorage.getItem("hour-12"));
+  $('#hour-01 .description').val(localStorage.getItem("hour-01"));
+  $('#hour-02 .description').val(localStorage.getItem("hour-02"));
+  $('#hour-03 .description').val(localStorage.getItem("hour-03"));
+  $('#hour-04 .description').val(localStorage.getItem("hour-04"));
+  $('#hour-05 .description').val(localStorage.getItem("hour-05")); 
   // TODO: Add code to display the current date in the header of the page.
-
